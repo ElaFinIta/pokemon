e7e7fa31bc19bcc687f38c87169c0d96e9f5f110
Ele Na <elena.china-kolehmainen@edu.bc.fi>
2022-01-18 23:00:22 +0200

    next and prev buttons, new get when hit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Pokemon app to recap CRA (create-react-app)
+
+An application to fetch information from the Pokemon API https://pokeapi.co/api/v2/pokemon/ to Pokédex. Name of the Pokemon, type/s and picture are rendered in cards.
+
+## Concepts practised:
+- react hooks
+- multiple API calls
+
+## Usage:
+
+Get 20 Pokemons at a time from the API. Next and prev buttons perform a new call.
+
+
+## Built with:
+- REACT
+- JXS
+- React Boostrap
+- axios
+- Pokemon API
+
+## Screenshot:
+
+Version 1:
+![screenshot](Screenshot.png?raw=true "Screenshot of the first version of Pokédex app")
+
+## To do:
+- [ ] compute weak against based on type
+
+## Credits:
+- Pokemon API: https://pokeapi.co/api/v2/pokemon/ 
+- Teacher Margit Tennosaat at BCH
--- src/App.js
+++ src/App.js
@@ -5,59 +5,81 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import axios from "axios";
+import Spinner from "react-bootstrap/Spinner";
+
+const API = 'https://pokeapi.co/api/v2/pokemon/';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [pokemons, setPokemons] = useState([]);
-  // const [posts, setPosts] = useState([]);
-
-  // useEffect(() => {
-  //   axios.get("https://pokeapi.co/api/v2/pokemon/")
-  //   .then(res => setPokemons(res.data.results))
-  // }, []);
-
+  const [next, setNext] = useState();
+  const [prev, setPrev] = useState();
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon/")
-      .then((res) => {
-        const fetches = res.data.results.map((poke) =>
-        axios.get(poke.url).then((singlePokeRes) => singlePokeRes.data)
-      );
-      Promise.all(fetches).then((data) => {
-        setPokemons(data);
-      });
-    });
+    getPokemons(API);
   }, []);
 
-  console.log('state after get:', pokemons);
-  pokemons.forEach(p => {
-    console.log('type', p.types.forEach(ob => ob.type.name));
-  })
+  const getPokemons = (endpoint) => {
+    setIsLoading(false);
+    axios.get(endpoint)
+      .then((res) => {
+        console.log(res.data.next);
+        setNext(res.data.next);
+        const fetches = res.data.results.map((poke) =>
+          axios.get(poke.url).then((singlePokeRes) => singlePokeRes.data)
+        );
+      Promise.all(fetches).then((data) => {
+        setPokemons(data);
+        setIsLoading(false);
+      });
+      if (pokemons[0]) {
+        // console.log("pokemons[0].id", pokemons[0].id);
+        setPrev(pokemons[0].id)
+      }
+      // console.log('starting id of previous get:', pokemons[0].id);
+    });
+  }
+
+
   return (
     <div>
       <Navbar variant="dark" bg="light" className="add_color">
         <Container>
-          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#">Pokédex</Navbar.Brand>
         </Container>
       </Navbar>
 
       <Container>
         <Row xs={1} md={3} lg={5} className="justify-content-between my-5 d-flex gap-3">
-          {pokemons.map((poke) => (
-              <Card style={{ width: '16rem'}}
+          {isLoading && (
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          )}
+          {!isLoading && pokemons.map((poke) => (
+              <Card style={{ width: '12rem'}}
                     key={poke.name}
                     bg='dark'
                     text="light">
+                <Card.Text>
+                  {poke.id}
+                </Card.Text>
                 <Card.Img src={poke.sprites.other.home.front_default}/>
                 <Card.Body>
-                  <Card.Title>{poke.name.toUpperCase()}</Card.Title>
+                  <Card.Title>{poke.name.toUpperCase()}
+                  </Card.Title>
                   <Card.Text>
-                    Type: {poke.types[0].type.name}
+                    Type: {poke.types.map((t, i) => (<li key={i}className="poke_type">{t.type.name} </li>))}
                   </Card.Text>
-                  <Button variant="info">Next</Button>
+                  <Card.Text>
+                    Weak against: {}
+                  </Card.Text>
                 </Card.Body>
               </Card>
             ))}
         </Row>
+        {next !== 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20' ? <Button variant="info" onClick={() => getPokemons(`https://pokeapi.co/api/v2/pokemon/?offset=${prev-1}&limit=20`)}>PREV</Button> : undefined}
+        {next !== 'https://pokeapi.co/api/v2/pokemon/?offset=1100&limit=18' ?<Button variant="info" onClick={() => getPokemons(next)}>NEXT</Button> : undefined}
       </Container>
     </div>
   );
